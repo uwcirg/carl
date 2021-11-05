e1caaae8869869fda558bfbf0f9b7495177fd3ae
Paul Bugni <pbugni@u.washington.edu>
2021-11-04 18:37:42 -0700

    make set worthy for easy lookups

--- carl/modules/coding.py
+++ carl/modules/coding.py
@@ -4,6 +4,7 @@ from carl.modules.resource import Resource
 
 class Coding(Resource):
     """FHIR Coding - used for serializing and queries"""
+    RESOURCE_TYPE = 'Coding'
 
     def __init__(self, code, system):
         super().__init__()
@@ -18,3 +19,13 @@ class Coding(Resource):
     def from_fhir(cls, data):
         """Deserialize from json (FHIR) data"""
         return cls(code=data['code'], system=data['system'])
+
+    def __eq__(self, other):
+        """Logical equals operator - needed for set functionality"""
+        return (
+            self._fields.get('code') == other._fields.get('code') and
+            self._fields.get('system') == other._fields.get('system'))
+
+    def __hash__(self):
+        """Generate logically unique hash for set functionality"""
+        return hash(f"{self._fields.get('system')}|{self._fields.get('code')}")
