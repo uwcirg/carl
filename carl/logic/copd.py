df6eea147f99e179a60ba9c2e9b2cf415107c364
Paul Bugni <pbugni@u.washington.edu>
2022-01-04 14:34:29 -0800

    lookup preferred identifier and use in logging

--- carl/logic/copd.py
+++ carl/logic/copd.py
@@ -1,5 +1,5 @@
 """COPD module"""
-from flask import current_app
+from flask import current_app, has_app_context
 import requests
 
 from carl.config import FHIR_SERVER_URL
@@ -18,6 +18,26 @@ CNICS_COPD_coding = Coding(
     system="https://cpro.cirg.washington.edu/groups",
     code="CNICS.COPD2021.11.001",
     display="COPD PRO group member")
+
+CNICS_IDENTIFIER_SYSTEM = "https://cnics.cirg.washington.edu/"
+
+
+def patient_canonical_identifier(patient_id):
+    """Return system|value identifier if patient has one for preferred system"""
+    search_params = {'patient': patient_id}
+    url = f"{FHIR_SERVER_URL}Patient"
+    response = requests.get(url, params=search_params)
+    if has_app_context():
+        current_app.logger.debug(f"HAPI GET: {response.url}")
+    response.raise_for_status()
+
+    match = [
+        f"{identifier['system']}|{identifier['value']}"
+        for identifier in response.json().get("identifier", [])
+        if identifier['system'] == CNICS_IDENTIFIER_SYSTEM]
+
+    if match:
+        return match[0]
 
 
 def patient_has(patient_id, value_set_uri):
@@ -63,6 +83,8 @@ def process_4_COPD(patient_id):
     """
     current_app.logger.debug(f"process {patient_id} for COPD")
     positive_codings = patient_has(patient_id=patient_id, value_set_uri=COPD_VALUESET_URI)
+    # Obtain and use canonical identifier for comparisons across systems, if found
+    patient_id = patient_canonical_identifier(patient_id) or patient_id
     results = {
         "patient_id": patient_id,
         "COPD codings found": len(positive_codings) > 0}
