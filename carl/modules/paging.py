78af701c73954d4c94f1f60a735daf3fc33cb1db
Paul Bugni <pbugni@u.washington.edu>
2023-07-26 15:58:24 -0700

    extend `next_resource_bundle` to handle Resource types as well as string.

--- carl/modules/paging.py
+++ carl/modules/paging.py
@@ -4,6 +4,7 @@ import jmespath
 import requests
 
 from carl.config import FHIR_SERVER_URL
+from carl.modules.resource import Resource
 
 
 def next_page_link_from_bundle(bundle):
@@ -18,12 +19,12 @@ def next_page_link_from_bundle(bundle):
 def next_resource_bundle(resource_type, search_params=None):
     """Generate pages of search results, yielding bundles until exhausted
 
-    :param resource_type: string form of resource to look up, i.e. `Patient`
+    :param resource_type: `Resource` object or string form of resource to look up, i.e. `Patient`
     :param search_params: optional search criteria to filter or order results
     :returns: bundle per page until exhausted
     """
-
-    url = f"{FHIR_SERVER_URL}{resource_type}"
+    resource_string = resource_type.RESOURCE_TYPE if isinstance(resource_type, Resource) else resource_type
+    url = f"{FHIR_SERVER_URL}{resource_string}"
     response = requests.get(url=url, params=search_params, timeout=30)
     if has_app_context():
         current_app.logger.debug(f"HAPI GET: {response.url}")
