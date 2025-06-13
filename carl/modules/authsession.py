ec050e574a7412d2cd82a3efcb0bbe1919f0a2e6
Paul F Bugni <pbugni@uw.edu>
2025-06-13 10:56:15 -0700

    must POST to /auth/token (duh!)

--- carl/modules/authsession.py
+++ carl/modules/authsession.py
@@ -19,7 +19,7 @@ def _fetch_token():
     # verify=False supress SSL warnings like curl --insecure  (NOT ADVISED)
     current_app.logger.debug(f"token request to {FHIR_SERVER_AUTH_URL}")
     current_app.logger.debug(f"with headers: {headers} and data: {body}")
-    response = requests.get(FHIR_SERVER_AUTH_URL, verify=False, headers=headers, data=body)
+    response = requests.post(FHIR_SERVER_AUTH_URL, verify=False, headers=headers, data=body)
     current_app.logger.debug(f"response code: {response.status_code}")
     # response.raise_for_status()  insecure raises a 988
     current_app.logger.debug(f"text: {response.text}") 
