de2c4bd850395e6301b9fb9f7298c9247b800c4c
Paul Bugni <pbugni@u.washington.edu>
2025-06-13 13:50:01 -0700

    enable use on servers with and without FHIR_SERVER_AUTH_URL. provide config where FHIR_SERVER doesn't have a valid certificat.

--- carl/modules/authsession.py
+++ carl/modules/authsession.py
@@ -6,6 +6,7 @@ from carl.config import (
     FHIR_CLIENT_ID,
     FHIR_CLIENT_SECRET,
     FHIR_SERVER_AUTH_URL,
+    FHIR_SERVER_IGNORE_CERT,
 )
 
 
@@ -16,8 +17,8 @@ def _fetch_token():
         "client_id": FHIR_CLIENT_ID,
         "client_secret": FHIR_CLIENT_SECRET,
     }
-    # verify=False supress SSL warnings like curl --insecure  (NOT ADVISED)
-    response = requests.post(FHIR_SERVER_AUTH_URL, verify=False, headers=headers, json=body)
+    verify = not FHIR_SERVER_IGNORE_CERT
+    response = requests.post(FHIR_SERVER_AUTH_URL, verify=verify, headers=headers, json=body)
     response.raise_for_status()
     data = response.json()
     assert data["token_type"] == "Bearer"
@@ -25,6 +26,9 @@ def _fetch_token():
 
 
 def dont_verify(**kwargs):
+    """Add verify=False parameter when server doesn't have valid certificate"""
+    if not FHIR_SERVER_IGNORE_CERT:
+        return kwargs
     updated_kwargs = dict(kwargs)
     updated_kwargs["verify"] = False
     return updated_kwargs
@@ -38,6 +42,10 @@ class AuthSession:
         self.session = requests.Session()
 
     def _update_token(self):
+        if not FHIR_SERVER_AUTH_URL:
+            # Ignore token need on servers without a configured AUTH_URL
+            return
+
         now = time.time()
         if not AuthSession._token or now >= AuthSession._token_expiration:
             current_app.logger.debug("Fetch auth token")
@@ -51,24 +59,20 @@ class AuthSession:
 
     def delete(self, url, **kwargs):
         self._update_token()
-        current_app.logger.debug(f"make authorized DELETE request to {url}")
         updated_kwargs = dont_verify(**kwargs)
         return self.session.delete(url, **updated_kwargs)
 
     def get(self, url, **kwargs):
         self._update_token()
-        current_app.logger.debug(f"make authorized GET request to {url}")
         updated_kwargs = dont_verify(**kwargs)
         return self.session.get(url, **updated_kwargs)
 
     def post(self, url, **kwargs):
         self._update_token()
-        current_app.logger.debug(f"make authorized POST request to {url}")
         updated_kwargs = dont_verify(**kwargs)
         return self.session.post(url, **updated_kwargs)
 
     def put(self, url, **kwargs):
         self._update_token()
-        current_app.logger.debug(f"make authorized PUT request to {url}")
         updated_kwargs = dont_verify(**kwargs)
         return self.session.put(url, **updated_kwargs)
